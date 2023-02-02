0a4f760a9e0d044ac2c9d356ad8cc0e95c034fd4
abrahamuchos <agonzalez@wayuinc.com>
2023-02-02 19:03:32 -0400

    Add base layout

--- src/router.jsx
+++ src/router.jsx
@@ -1,27 +1,51 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 
 import Login from "./views/Login";
 import NotFound from "./views/NotFound";
 import Signup from "./views/Signup";
 import Users from "./views/Users";
+import Dashboard from './views/Dashboard'
 
 const router = createBrowserRouter([
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
-    },
-    {
-        path: '*',
-        element: <NotFound />
-    },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+        {
+            path: '/',
+            element: <Navigate to='users'/>
+        },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
