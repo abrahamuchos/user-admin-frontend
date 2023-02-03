c356afa7c6d927a15fa94ea49b787e80edd18cdb
abrahamuchos <agonzalez@wayuinc.com>
2023-02-03 17:25:42 -0400

    Add styles and structure to default laytout

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 function DefaultLayout() {
   const { user, token } = useStateContext();
 
-  if(!token){
-    return <Navigate to='/login'/>
+  if (!token) {
+    return <Navigate to='/login' />;
+  }
+
+  const handleLogout = (e) =>{
+    e.preventDefault();
   }
 
   return (
-    <div>
-      <Outlet />
+    <div id='defaultLayout'>
+      <aside>
+        <Link to='/dashboard'>Dashboard</Link>
+        <Link to='/users'>Users</Link>
+      </aside>
+      <div className='content'>
+        <header>
+          <div>Header</div>
+          <div>
+            {user.name}
+          </div>
+          <a href="#" onClick={handleLogout} className='btn-logout'>Logout</a>
+        </header>
+        <main>
+            <Outlet />
+          </main>
+      </div>
     </div>
   );
 }
