372025d7e0cad8aebd0e6056fd7c5bf0ee66453e
abrahamuchos <agonzalez@wayuinc.com>
2023-02-02 21:50:34 -0400

    Add context provaider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { ContextProvider } from "./contexts/ContextProvider";
 
-import './index.css'
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
+  </React.StrictMode>
+);
